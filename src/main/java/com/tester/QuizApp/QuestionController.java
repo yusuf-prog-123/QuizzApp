276274af7f0878af9cf11bea346426222757b288
unknown <yusufmcgmcg@gmail.com>
2025-03-02 18:16:46 +0530

    6th commit intordution to status code like 404 and resposne entity

--- src/main/java/com/tester/QuizApp/QuestionController.java
+++ src/main/java/com/tester/QuizApp/QuestionController.java
@@ -3,6 +3,7 @@ package com.tester.QuizApp;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,18 +19,18 @@ public class QuestionController {
     questionserrvice questionService;
 
 	@GetMapping("/allQuestion")
-	public List<ModelClass> getallQuestion() {
+	public ResponseEntity<List<ModelClass>> getallQuestion() {
 		
 		return questionService.getAllQuestions();//A method in service class or model class this where operations happenss for different request by the client
 		
 	}
 	@GetMapping("/catagory/{cat}")
-	public List<ModelClass> getquestbycatagoryContoller(@PathVariable("cat") String catagory){
+	public ResponseEntity<List<ModelClass>> getquestbycatagoryContoller(@PathVariable("cat") String catagory){
 		
 		return questionService.getquestbycatagory(catagory);
 	}
 	@PostMapping("/add")
-	public String addQuest(@RequestBody ModelClass quest) {
+	public ResponseEntity<String> addQuest(@RequestBody ModelClass quest) {
 		return questionService.addingQuest(quest);
 		
 	}
--- src/main/java/com/tester/QuizApp/Service/questionserrvice.java
+++ src/main/java/com/tester/QuizApp/Service/questionserrvice.java
@@ -4,6 +4,9 @@ import com.tester.QuizApp.Dao.Daoooo;
 import com.tester.QuizApp.ModelClass;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,20 +17,23 @@ public class questionserrvice {
 	
 	
 
-	public List<ModelClass> getAllQuestions() {
+	public ResponseEntity<List<ModelClass>> getAllQuestions() {
 		// TODO Auto-generated method stub
-		return dao1.findAll();
+		return new ResponseEntity<>(dao1.findAll(),HttpStatus.OK);
 	}
 
-	public List<ModelClass> getquestbycatagory(String catagory) {
+	public ResponseEntity<List<ModelClass>> getquestbycatagory(String catagory) {
 		// TODO Auto-generated method stub
-		return dao1.findBydifficultylevel(catagory);
+		return new ResponseEntity<List<ModelClass>>( dao1.findBydifficultylevel(catagory),HttpStatus.BAD_GATEWAY);
+		/*check out buy requesting a get "http://localhost:8080/question/catagory/Easy" eventhough you got the response n
+		 * postman it will show "502 Bad Gateway" this because we aded "HttpStatus.BAD_GATEWAY" if the function 
+		 * succesfully executed*/
 	}
 
-	public String addingQuest(ModelClass questi) {
+	public ResponseEntity<String> addingQuest(ModelClass questi) {
 		// TODO Auto-generated method stub
 		dao1.save(questi);
-		return "Success";
+		return new ResponseEntity<String>("Success",HttpStatus.CREATED);
 	}
 	
 
